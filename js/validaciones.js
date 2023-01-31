974e66a6aa8b881eaf4de771a2f7dcf4daa7a746
margajm <margaritajunco2020@gmail.com>
2023-01-31 00:17:21 -0500

    Clase Mostrando Mensajes de Error

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,5 +1,8 @@
 //Este input estará vinculado a otro archivo que iremos generando
 //valida va a recibir el input, va a verificar cuál es el tipo de input a través de dataset.tipo 
+// valid verifica si está valido o no lo que el usuario ingresó en el input.
+//y dependiendo de eso agregar o quitar la clase CSS que hace que se ponga rojo el input como inválido.
+//esta función se manda llamar cada vez que el usuario sale del input que estaba rellenando.
 //exportamos para usarla en otros archivos
 export function valida(input){
     //Dataset lo que nosotros estamos obteniendo es la colección de todos los datas atribute
@@ -8,7 +11,55 @@ export function valida(input){
     //Necesitamos por cada uno de los tipos de input o por el tipo de input, verificar si existe dentro de los validadores.
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input);
-    }
+    };
+
+    //Verificaremos con esto si lo que está dentro del input es válido.
+    //si está en true, quiero que quite la clase del rojito que avisa que está mal lo que puso el usuario.
+    //Validity:  Devuelve un objeto ValidityState que contiene varias propiedades que describen el estado de validez del elemento.
+    //Valid:Devuelve true si el elemento cumple con todas sus restricciones de validación y por lo tanto se considera válido, 
+    //o false si falla alguna restricción. 
+    if(input.validity.valid){
+        //si está en true (es válido) quiero que quite la clase CSS que indica que es inválido lo que puso el usuario.
+        input.parentElement.classList.remove("input-container--invalid");
+
+    } else {
+        // Si esta en false o sea es inválido lo que escribio en el input el usuario.
+        //Entonces tenemos que agregar o poner la clase al padre del input en este caso al div.
+        //la clase CCS para que represente que está mal o es inválido. 
+        input.parentElement.classList.add("input-container--invalid");
+
+    };
+};
+
+//Esto nos ayudará con los tipos de errores de cada uno de los inputs de nuestro formulario.
+//Para cada tipo de error mostraremos un mensaje diferente.
+const mensajesDeError =  {
+    //Tenemos primero al objeto del input del nombre.
+    nombre: {
+        //Con ValueMissing verificamos si el valor está faltando si el usuario no puso nada allí.
+        valueMissing:"Este campo no puede estar vacío."
+    },
+
+    email: {
+        //Con ValueMissing verificamos si el valor está faltando si el usuario no puso nada allí.
+        valueMissing:"Este campo no puede estar vacío.",
+        //typeMissmatch hace referencia a que si es un correo electrónico.
+        typeMissmatch: "El correo no es válido.",
+    },
+
+    password: {
+        //Con ValueMissing verificamos si el valor está faltando si el usuario no puso nada allí.
+        valueMissing:"Este campo no puede estar vacío.",
+        patternMissmatch:"Al menos 6 caracteres, un máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
+
+    },
+
+    nacimiento: {
+        //Con ValueMissing verificamos si el valor está faltando si el usuario no puso nada allí.
+        valueMissing:"Este campo no puede estar vacío.",
+        customError: "Debes tener al menos 18 años de edad",
+    },
+
 };
 
 const validadores = {
