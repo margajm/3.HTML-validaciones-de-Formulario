7b6e45c5f6468da3ce0af0f2e499ccbbc7f17b77
margajm <margaritajunco2020@gmail.com>
2023-01-31 11:10:28 -0500

    Aula04 completada

--- js/validaciones.js
+++ js/validaciones.js
@@ -6,7 +6,7 @@
 //exportamos para usarla en otros archivos
 export function valida(input){
     //Dataset lo que nosotros estamos obteniendo es la colección de todos los datas atribute
-    //El punto tipo es para obtener el de la fecha de nacimiento que le pusimos como nombre tipo.
+    //El punto tipo es para obtener el tipo de Input.
     const tipoDeInput = input.dataset.tipo;
     //Necesitamos por cada uno de los tipos de input o por el tipo de input, verificar si existe dentro de los validadores.
     if(validadores[tipoDeInput]){
@@ -14,22 +14,38 @@ export function valida(input){
     };
 
     //Verificaremos con esto si lo que está dentro del input es válido.
-    //si está en true, quiero que quite la clase del rojito que avisa que está mal lo que puso el usuario.
     //Validity:  Devuelve un objeto ValidityState que contiene varias propiedades que describen el estado de validez del elemento.
     //Valid:Devuelve true si el elemento cumple con todas sus restricciones de validación y por lo tanto se considera válido, 
     //o false si falla alguna restricción. 
     if(input.validity.valid){
         //si está en true (es válido) quiero que quite la clase CSS que indica que es inválido lo que puso el usuario.
-        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.classList.remove("input-container--invalid");     
+        //Si es true quiero que el contenido de ese span de mensajes de errora sea igual a un string vacío. 
+        input.parentElement.querySelector(".input-message-error").innerHTML="";
 
     } else {
         // Si esta en false o sea es inválido lo que escribio en el input el usuario.
         //Entonces tenemos que agregar o poner la clase al padre del input en este caso al div.
         //la clase CCS para que represente que está mal o es inválido. 
         input.parentElement.classList.add("input-container--invalid");
+        //En caso de que si existe algún error, lo que quiero es mostrar mensaje de error llamando a esa funcion, para que se entienda mejor.
+        //Esta funcion de mostrarMensaje de error su primer parametro es el tipo de input y el segundo el input.
+        input.parentElement.querySelector(".input-message-error").innerHTML= mostrarMensajeDeError(tipoDeInput,input);
 
     };
 };
+
+//Este arreglo nos ayudará a interceptar dependiendo del tipo de input, para cada tipo de errores, lo voy a recorrer.
+const tipoDeErrores = [
+    //Error de que falta el valor(el usuario dejo en blanco el campo).
+    "valueMissing",
+    //Error de que no  coincide con el type de input que definimos en el HTML ejemplo type = "email",
+    "typeMismatch",
+    //Error de que no coincide con el patron que pusimos por ejemplo un regex pattern
+    "patternMismatch",
+    //Error personalizado que configuramos para validar algo.
+    "customError",
+];
 
 //Esto nos ayudará con los tipos de errores de cada uno de los inputs de nuestro formulario.
 //Para cada tipo de error mostraremos un mensaje diferente.
@@ -37,26 +53,26 @@ const mensajesDeError =  {
     //Tenemos primero al objeto del input del nombre.
     nombre: {
         //Con ValueMissing verificamos si el valor está faltando si el usuario no puso nada allí.
-        valueMissing:"Este campo no puede estar vacío."
+        valueMissing:"El campo nombre no puede estar vacío.",
     },
 
     email: {
         //Con ValueMissing verificamos si el valor está faltando si el usuario no puso nada allí.
-        valueMissing:"Este campo no puede estar vacío.",
+        valueMissing:"El campo correo no puede estar vacío.",
         //typeMissmatch hace referencia a que si es un correo electrónico.
-        typeMissmatch: "El correo no es válido.",
+        typeMismatch:"El correo no es válido",
     },
 
     password: {
         //Con ValueMissing verificamos si el valor está faltando si el usuario no puso nada allí.
-        valueMissing:"Este campo no puede estar vacío.",
-        patternMissmatch:"Al menos 6 caracteres, un máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
+        valueMissing:"El campo contraseña no puede estar vacío.",
+        patternMismatch:"Al menos 6 caracteres, un máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.",
 
     },
 
     nacimiento: {
         //Con ValueMissing verificamos si el valor está faltando si el usuario no puso nada allí.
-        valueMissing:"Este campo no puede estar vacío.",
+        valueMissing:"El campo fecha de nacimiento no puede estar vacío.",
         customError: "Debes tener al menos 18 años de edad",
     },
 
@@ -66,6 +82,30 @@ const validadores = {
     //Vemos que coincidan el nombre del tipo = nacimiento con la llave dentro de este objeto que es una arrow funcion.
     //Una función que va a recibir input y lo que va a hacer es mandar a llamar validarNacimiento. 
     nacimiento: (input) => validarNacimiento(input),
+};
+
+//Esta funcion de mostrarMensaje de error su primer parametro es el tipo de input y el segundo el input.
+function mostrarMensajeDeError(tipoDeInput, input) {
+    //Tenemos un mensaje que es el que queremos decirle al usuario dependiendo de si el campo está vacío, 
+    let mensaje ="";
+    //En este tipo de errores voy a recibir cada uno de los errores que está en el arreglo..
+    tipoDeErrores.forEach((error) => {
+        //Vamos a buscar con validity si dentro de nuestro input hay alguno de los errores del arreglo.
+        if(input.validity[error]){
+            //Con esto vemos cuál tipo de Input y cual es el error específico
+            console.log(tipoDeInput, error);
+            //Con esto validamos si es que hay algun error.
+            console.log(input.validity[error]);
+            //Necesitamos ver cual es el tipo de Input que nos dió error y acceder a los mensajesDeError con sus valores.
+            console.log(mensajesDeError[tipoDeInput][error]);
+            // El mensaje va a ser igual a nuestro objeto donde están todos los mensajes de error para cada tipo de input.
+            //luego entre corchetes, el tipo de input para poder acceder a si es nombre, email, password, nacimiento,  y por último el error.
+            mensaje = mensajesDeError[tipoDeInput][error];
+        };
+
+    })
+
+    return mensaje;
 };
 
 //Creamos una funcion para validar la fecha de nacimiento 
